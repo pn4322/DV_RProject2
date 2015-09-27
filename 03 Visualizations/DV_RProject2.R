db914b0fad4f6c0b79cc7d899ade9dc4a49c6b92
pn4322 <prarthana_neotia@hotmail.com>
2015-09-27 18:29:42 -0500

    no message

--- 03 Visualizations/DV_RProject2.R
+++ 03 Visualizations/DV_RProject2.R
@@ -1,7 +1,7 @@
 require("jsonlite")
 require("RCurl")
 # Change the USER and PASS below to be your UTEid
-df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from NewYork where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_pn4322', PASS='orcl_pn4322', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from NewYork"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_pn4322', PASS='orcl_pn4322', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 df
 summary(df)
 head(df)
@@ -9,36 +9,44 @@ head(df)
 require(extrafont)
 ggplot() + 
   coord_cartesian() + 
-  scale_x_discrete() +
-  scale_y_continuous() +
-  facet_wrap(~ETHNICITY) +
+  scale_x_continuous() +
+  scale_y_discrete() +
+  #facet_wrap(~ETHNICITY) +
   #facet_grid(.~ETHNICITY, labeller=label_both) + # Same as facet_wrap but with a label.
-  #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+  facet_grid(ETHNICITY~YEAR_, labeller=label_both) +
   labs(title='NewYork') +
-  labs(x="Ethnicity", y=paste("Year")) +
+  labs(x="Year", y=paste("Ethnicity")) +
   layer(data=df, 
-        mapping=aes(x=(as.character(SEX)), y=as.numeric(as.character(YEAR_)), color=SEX), 
+        mapping=aes(x=as.numeric(as.character(YEAR_)), y=(as.character(ETHNICITY)), color=SEX), 
         stat="identity", 
         stat_params=list(), 
         geom="jitter",
         geom_params=list(), 
         #position=position_identity()
-        position=position_jitter(width=1, height=1)
+        position=position_jitter(width=0.7, height=0.7)
   )
+
+
+require("jsonlite")
+require("RCurl")
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from NewYork where sex is FEMALE"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_pn4322', PASS='orcl_pn4322', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df
+summary(df)
+head(df)
 
 ggplot() + 
   coord_cartesian() + 
   scale_x_discrete() +
-  scale_y_continuous() +
+  scale_y_discrete() +
   #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
-  labs(title='Titanic') +
-  labs(x="SURVIVED", y=paste("FARE")) +
+  labs(title='NewYork') +
+  labs(x="CAUSE", y=paste("SEX")) +
   layer(data=df, 
-        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
+        mapping=aes(x=(as.character(CAUSE)), y=(as.character(SEX)), color=as.character(ETHNICITY)), 
         stat="identity", 
         stat_params=list(), 
         geom="point",
         geom_params=list(), 
         #position=position_identity()
-        position=position_jitter(width=0.3, height=0)
+        position=position_jitter(width=0.5, height=0.5)
   )
