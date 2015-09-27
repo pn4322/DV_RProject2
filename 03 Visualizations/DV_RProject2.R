7393c39b2b6daa5b1178e5ee9b96bef0c085101b
pn4322 <prarthana_neotia@hotmail.com>
2015-09-27 17:27:51 -0500

    no message

---
+++ 03 Visualizations/DV_RProject2.R
@@ -0,0 +1,44 @@
+require("jsonlite")
+require("RCurl")
+# Change the USER and PASS below to be your UTEid
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from NewYork where sex is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_pn4322', PASS='orcl_pn4322', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df
+summary(df)
+head(df)
+
+require(extrafont)
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  facet_wrap(~ETHNICITY) +
+  #facet_grid(.~ETHNICITY, labeller=label_both) + # Same as facet_wrap but with a label.
+  #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+  labs(title='NewYork') +
+  labs(x="Ethnicity", y=paste("Year")) +
+  layer(data=df, 
+        mapping=aes(x=(as.character(SEX)), y=as.numeric(as.character(YEAR_)), color=SEX), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="jitter",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=1, height=1)
+  )
+
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+  labs(title='Titanic') +
+  labs(x="SURVIVED", y=paste("FARE")) +
+  layer(data=df, 
+        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        #position=position_identity()
+        position=position_jitter(width=0.3, height=0)
+  )
