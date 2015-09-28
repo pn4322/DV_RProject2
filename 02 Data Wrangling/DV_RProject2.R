e8ce523c183d57794394bc490ce01ab79d30f406
pn4322 <prarthana_neotia@hotmail.com>
2015-09-28 16:10:15 -0500

    no message

--- 02 Data Wrangling/DV_RProject2.R
+++ 02 Data Wrangling/DV_RProject2.R
@@ -7,7 +7,7 @@ file_path <- "NewYork.csv"
 #df <- rename(NewYork, tbl = table)
 df <- read.csv(file_path)
 tbl_df(df)
-View(df)
+#View(df)
 
 # select
 df %>% select(Ethnicity, Year_) %>% tbl_df
@@ -17,6 +17,6 @@ df %>% select(Year_, Sex) %>% filter(Year_ < 2009) %>% tbl_df
 
 
 
-df %>% group_by(Year_,Count_) %>% summarise(mean = mean(Count_))
+df %>% group_by(Ethnicity,Percent_) %>% summarise(mean = mean(Percent_))
 
 
